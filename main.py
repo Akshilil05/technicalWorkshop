74938885e2a08ce109ef16461b9d3289b31b6b65
Akshilil05 <akshithatarigoppula@gmail.com>
2025-03-21 12:46:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import streamlit as st
 
 
-st.title('Hello World')
-st.write('This is a simple Streamlit app.')
+#st.title('Hello World')
+#st.write('This is a simple Streamlit app.')
 
 
-if st.button('Say hello'):
-   st.text('Hello, Streamlit!')
+#if st.button('Say hello'):
+   #st.text('Hello, Streamlit!')
 
 
-name = st.text_input('Please enter your name:')
-if name:
-   st.write(f'Hello, {name}!')
+#name = st.text_input('Please enter your name:')
+#if name:
+   #st.write(f'Hello, {name}!')
 
 
-if st.file_uploader('Please upload a file:', type=['txt', 'csv']):
-   st.write('Thanks for uploading a file!')
+#if st.file_uploader('Please upload a file:', type=['txt', 'csv']):
+   #st.write('Thanks for uploading a file!')
+name = st.text_input("Enter your name", "John Doe")
+password = st.text_input("Enter your password", type="password")
+
+
+if st.button("Submit"):
+   if len(password) < 8:
+       st.warning("Password must be at least 8 characters long")
+
+
+   st.success("Password is valid")
